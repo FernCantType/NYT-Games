18c61c96a2a9ebb2f16df91837a278b1f852fe73
FernCantType <lorenzocanali@icloud.com>
2025-04-20 15:50:44 -0500

    made super abstract class just need to actually make it

---
+++ NYTGames.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * A super class that contains the words so I only need to read the file once while storing the
+ * words for all the classes
+ * 
+ * @author Lorenzo Canali
+ * @version 4/20/2025
+ * @updated
+ */
+public abstract class NYTGames {
+    private static ArrayList<String> allWords;
+
+    //Constructor method
+    //words.txt file read in method
+    //Sort abstract method
+    //Menu method
+}
